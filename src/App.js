2f6a0dfa758d96b2a41715022e7907f5ba0e85ff
Himanshu <hmanshugupta1@gmail.com>
2018-12-07 00:07:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {connect} from 'react-redux';
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.onAddClick = this.onAddClick.bind(this);
+  }
+
+  onAddClick(){
+
+    let item  = {text: "hello world: "+this.props.todoList.length+"#"};
+    this.props.onAddClick(item);
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,13 +26,17 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <button onClick={this.onAddClick}>hello</button>
+
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+          <p>{JSON.stringify(this.props)}</p>
+
+            Learn React:
           </a>
         </header>
       </div>
@@ -25,4 +44,24 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = function(state) {
+  return state;
+}
+
+const mapDispatchToProps = function(dispatch){
+  return{
+    onAddClick: function(item){
+      dispatch(addToDoAction(item));
+    }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
+
+
+const addToDoAction = function(item){
+  return ({
+    type: "ADD_TODO",
+    payload: item
+  });
+}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+
+
+const todoReducer = function(state={todoList: []},action){
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TODO":
+      // let l = state.todolist;
+      // l.push(action.payload.text);
+      // return {...state,
+      //   todolist: l
+      // }
+      return {...state, todoList:  [...state.todoList,action.payload] };
+    default:
+      return state;
+  }
+}
+
+let store = createStore(todoReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
